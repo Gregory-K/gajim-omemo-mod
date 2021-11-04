ebc249e0c551d2f280215cad54c08dbc16bcc462
Gregory.K <gk@makemagic.gr>
2021-11-04 21:37:27 +0100

    ^ Sync with source v2.7.15

--- file_crypto.py
+++ file_crypto.py
@@ -110,7 +110,7 @@ class FileDecryption:
         if transfer.size:
             # This gets called even when the requested file is not found
             # So only update the progress if the file was actually found and
-            # we know the size 
+            # we know the size
             transfer.update_progress()
 
         self._session.pause_message(message)
@@ -124,7 +124,8 @@ class FileDecryption:
         if message.status_code != Soup.Status.OK:
             log.warning('Download failed: %s', transfer.request_uri)
             log.warning(Soup.Status.get_phrase(message.status_code))
-            transfer.set_error('http-error', 'Download failed: %s', transfer.request_uri)
+            error_text = _('Download failed: %s') % transfer.request_uri
+            transfer.set_error('http-error', error_text)
             return
 
         data = message.props.response_body_data.get_data()
